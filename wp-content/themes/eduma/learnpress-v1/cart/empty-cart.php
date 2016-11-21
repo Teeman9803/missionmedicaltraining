4e93afa02703d1d30e27153cfa30d280ae15f0b2
terrallewis <tldesignsprintandweb@gmail.com>
2016-11-20 20:10:32 -0500

    updated checkout text

--- wp-content/themes/eduma/learnpress-v1/cart/empty-cart.php
+++ wp-content/themes/eduma/learnpress-v1/cart/empty-cart.php
@@ -13,4 +13,4 @@ learn_press_add_notice( __( 'Your cart is currently empty.', 'eduma' ), 'error' 
 
 learn_press_print_notices();
 ?>
-<a href="<?php echo learn_press_get_page_link( 'courses' ); ?>"><?php _e( 'Back to class', 'eduma' ); ?></a>
+<a href="<?php echo learn_press_get_page_link( 'courses' ); ?>"><?php _e( 'Back to courses', 'eduma' ); ?></a>
